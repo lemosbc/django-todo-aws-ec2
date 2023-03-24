61fee05ea76574bd89ccbbda13482240f4c458b1
Beatriz Lemos <10408674+lemosbc@users.noreply.github.com>
2023-03-24 23:54:35 +0000

    Create Tasks model

--- todo_list/base/models.py
+++ todo_list/base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User # imports built-in User model
 
 # Create your models here.
+
+class Taks(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['complete']
